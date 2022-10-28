70fe7e0d916274a97dca3033f8592d6ae61537c2
Nate <nathandcwendy@gmail.com>
2022-10-28 01:33:47 +0100

    added a router link to the header for faster navigation to home page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import { React, useContext } from "react";
 import { IoMoonOutline, IoMoonSharp } from "react-icons/io5";
 import ThemeContext from "../contexts/ThemeContext";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const { theme, setTheme } = useContext(ThemeContext);
   return (
     <header className="sync-padding w-full flex justify-between items-center bg-white dark:bg-dm-dBlue py-6 shadow-custom-4 shadow-shadow-custom  dark:text-white">
-      <h1 className="text-base ssm:text-2xl mdsm:text-2xl font-extrabold">
-        Where in the world?
-      </h1>
+      <Link to={"/"}>
+        <h1 className="text-base ssm:text-2xl mdsm:text-2xl font-extrabold">
+          Where in the world?
+        </h1>
+      </Link>
       <div
         className="flex gap-x-2 items-center hover:cursor-pointer"
         onClick={() => {
